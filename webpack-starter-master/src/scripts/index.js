45a0d7c554fe1e84b42627ecd93b642bc3387879
Wesly Backers <wesly.backers@gmail.com>
2019-10-07 20:37:20 +0200

    first location in map

--- webpack-starter-master/src/scripts/index.js
+++ webpack-starter-master/src/scripts/index.js
@@ -1,3 +1,8 @@
+import L from 'leaflet';
+import "leaflet/dist/leaflet.css";
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+var mymap = L.map('mapid').setView([51.1925274,3.2202106], 13);
+L.marker([51.1925274,3.2202106]).addTo(mymap);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
